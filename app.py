5797b6495414a16266b80872ec64adb5c86156ea
bibekpreet99 <43917624+bibekpreet99@users.noreply.github.com>
2020-08-26 13:51:27 +0530

    Homepage

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, Markup
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from tempfile import mkdtemp
+from cs50 import SQL
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+app.debug = True
+
+db = SQL("sqlite:///decimal.db")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
