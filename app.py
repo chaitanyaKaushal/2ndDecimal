6859028e39048e8ae4bf687b7fc84f71ac05e1dd
bibekpreet99 <43917624+bibekpreet99@users.noreply.github.com>
2020-09-01 19:22:44 +0530

    login button

--- app.py
+++ app.py
@@ -26,3 +26,7 @@ db = SQL("sqlite:///decimal.db")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
