300cd1733deba50a34702a6eeae331c68c47921b
chaitanyaKaushal <ckaushal2028@gmail.com>
2020-11-06 12:20:45 +0530

    Schedule,announcements,cmaterial tables constructed

--- database/database.py
+++ database/database.py
@@ -4,35 +4,71 @@ conn = sqlite3.connect('decimal.db')
 
 c = conn.cursor()
 
-c.execute(""" create table student(
-    id TEXT primary key,
-    roll_no TEXT,
-    name TEXT,
-    email TEXT,
-    sem TEXT,
-    year TEXT,
-    branch TEXT,
-    batch TEXT,
-    passwd TEXT
-)
-""")
+# c.execute(""" create table student(
+#     id TEXT primary key,
+#     roll_no TEXT,
+#     name TEXT,
+#     email TEXT,
+#     sem TEXT,
+#     year TEXT,
+#     branch TEXT,
+#     batch TEXT,
+#     passwd TEXT
+# )
+# """)
 
-c.execute(""" create table teacher(
-    reg_id TEXT primary key,
-    name TEXT,
-    email TEXT,
+# c.execute(""" create table teacher(
+#     reg_id TEXT primary key,
+#     name TEXT,
+#     email TEXT,
+#     subject TEXT,
+#     passwd TEXT
+# )
+# """)
+
+# c.execute(""" create table course(
+#     id TEXT primary key,
+#     course_name TEXT,
+#     year TEXT,
+#     branch TEXT,
+#     sem TEXT
+# )
+# """)
+
+c.execute(""" create table announcement(
+    id TEXT primary key,
+    tid TEXT,
     subject TEXT,
-    passwd TEXT
+    cid TEXT,
+    upper TEXT,
+    lower TEXT,
+    info TEXT
 )
+
 """)
 
-c.execute(""" create table course(
+c.execute(""" create table schedule(
     id TEXT primary key,
-    course_name TEXT,
-    year TEXT,
-    branch TEXT,
-    sem TEXT
+    tid TEXT,
+    subject TEXT,
+    cid TEXT,
+    upper TEXT,
+    lower TEXT,
+    info TEXT
 )
+
+""")
+
+c.execute(""" create table cmaterial(
+    id TEXT primary key,
+    tid TEXT,
+    subject TEXT,
+    cid TEXT,
+    upper TEXT,
+    lower TEXT,
+    info TEXT
+)
+
 """)
 
 conn.commit()
