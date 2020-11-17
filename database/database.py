b2eb506b86439c6637d585c6f88cbce987d02373
chaitanyaKaushal <ckaushal2028@gmail.com>
2020-11-17 20:10:01 +0530

    testing changes

--- database/database.py
+++ database/database.py
@@ -4,36 +4,36 @@ conn = sqlite3.connect('decimal.db')
 
 c = conn.cursor()
 
-# c.execute(""" create table student(
-#     id TEXT primary key,
-#     roll_no TEXT,
-#     name TEXT,
-#     email TEXT,
-#     sem TEXT,
-#     year TEXT,
-#     branch TEXT,
-#     batch TEXT,
-#     passwd TEXT
-# )
-# """)
+c.execute(""" create table student(
+    id TEXT primary key,
+    roll_no TEXT,
+    name TEXT,
+    email TEXT,
+    sem TEXT,
+    year TEXT,
+    branch TEXT,
+    batch TEXT,
+    passwd TEXT
+)
+""")
 
-# c.execute(""" create table teacher(
-#     reg_id TEXT primary key,
-#     name TEXT,
-#     email TEXT,
-#     subject TEXT,
-#     passwd TEXT
-# )
-# """)
+c.execute(""" create table teacher(
+    reg_id TEXT primary key,
+    name TEXT,
+    email TEXT,
+    subject TEXT,
+    passwd TEXT
+)
+""")
 
-# c.execute(""" create table course(
-#     id TEXT primary key,
-#     course_name TEXT,
-#     year TEXT,
-#     branch TEXT,
-#     sem TEXT
-# )
-# """)
+c.execute(""" create table course(
+    id TEXT primary key,
+    course_name TEXT,
+    year TEXT,
+    branch TEXT,
+    sem TEXT
+)
+""")
 
 c.execute(""" create table announcement(
     id TEXT primary key,
