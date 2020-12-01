d30f90dc2cc8928f818afe7ae618b28d851d82e2
Bibekpreet Singh <bibekpreet99@gmail.com>
2020-12-01 22:51:40 +0530

    some changes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def after_request(response):
     return response
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 app.debug = True
